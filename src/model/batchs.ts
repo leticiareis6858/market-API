b83c4f177fec686618208421e0edc3e414d1a1cc
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-05-15 01:09:25 -0300

    style: fix typo in the word batches

--- src/model/batchs.ts
+++ src/model/batchs.ts
@@ -9,24 +9,24 @@ interface IBatch {
 
 export const createBatch = async (products: IProduct[]): Promise<IBatch> => {
   const querytText =
-    "INSERT INTO product-batchs (products, creation_date) VALUES ($1, CURRENT_TIMESTAMP) RETURNING *";
+    "INSERT INTO product-batches (products, creation_date) VALUES ($1, CURRENT_TIMESTAMP) RETURNING *";
   const { rows } = await pool.query(querytText, [products]);
   return rows[0];
 };
 
-export const getAllBatchs = async (): Promise<IBatch[]> => {
-  const { rows } = await pool.query("SELECT * FROM product-batchs");
+export const getAllBatches = async (): Promise<IBatch[]> => {
+  const { rows } = await pool.query("SELECT * FROM product-batches");
   return rows;
 };
 
 export const getBatchById = async (id: number): Promise<IBatch> => {
   const { rows } = await pool.query(
-    "SELECT * FROM product-batchs WHERE id = $1"
+    "SELECT * FROM product-batches WHERE id = $1"
   );
   return rows[0];
 };
 
 export const deleteBatch = async (id: number): Promise<void> => {
-  const queryText = "DELETE FROM product-batchs WHERE id = $1";
+  const queryText = "DELETE FROM product-batches WHERE id = $1";
   await pool.query(queryText, [id]);
 };
