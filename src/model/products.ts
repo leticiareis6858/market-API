3739d5f52379d2aeb3dcd313ce09b987700f86e7
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-05-15 00:30:28 -0300

    feat: add createProduct function

--- src/model/products.ts
+++ src/model/products.ts
@@ -6,3 +6,14 @@ interface IProduct {
   expiration_date: Date;
   batch: number;
 }
+
+export const createProduct = async (
+  name: string,
+  expiration_date: Date,
+  batch: number
+): Promise<IProduct> => {
+  const queryText =
+    "INSERT INTO products (name, expiration_date, batch) values ($1, $2, $3) RETURNING *";
+  const { rows } = await pool.query(queryText, [name, expiration_date, batch]);
+  return rows[0];
+};
