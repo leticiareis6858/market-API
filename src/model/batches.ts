42fc4dedbc8a2e0a10db066ac7ec5914b20cbf3e
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-05-15 22:27:28 -0300

    feat: Add unit buying and selling prices to batch model and functions

--- src/model/batches.ts
+++ src/model/batches.ts
@@ -4,6 +4,8 @@ import { IProduct } from "./products";
 export interface IBatch {
   id: number;
   product_name: string;
+  unit_buying_price: number;
+  unit_selling_price: number;
   creation_date: Date;
   expiration_date: Date;
   products: IProduct[];
@@ -11,6 +13,8 @@ export interface IBatch {
 
 export const createsBatch = async (
   product_name: string,
+  unit_buying_price: number,
+  unit_selling_price: number,
   expiration_date: Date,
   quantity: number
 ): Promise<IBatch> => {
@@ -19,16 +23,22 @@ export const createsBatch = async (
     await client.query("BEGIN");
 
     const batchResult = await client.query(
-      "INSERT INTO product_batches (product_name, creation_date, expiration_date) VALUES ($1, CURRENT_TIMESTAMP, $2) RETURNING *",
-      [product_name, expiration_date]
+      "INSERT INTO product_batches (product_name, unit_buying_price, unit_selling_price, creation_date, expiration_date) VALUES ($1, $2, $3, CURRENT_TIMESTAMP, $4) RETURNING *",
+      [product_name, unit_buying_price, unit_selling_price, expiration_date]
     );
     const batch = batchResult.rows[0];
 
     const products: IProduct[] = [];
     for (let i = 0; i < quantity; i++) {
       const productResult = await client.query(
-        "INSERT INTO products (name, expiration_date, batch_id) VALUES ($1, $2, $3) RETURNING *",
-        [product_name, expiration_date, batch.id]
+        "INSERT INTO products (name, unit_buying_price, unit_selling_price, expiration_date, batch_id) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+        [
+          product_name,
+          unit_buying_price,
+          unit_selling_price,
+          expiration_date,
+          batch.id,
+        ]
       );
       const product = productResult.rows[0];
       products.push(product);
@@ -59,13 +69,17 @@ export const getsBatchById = async (id: number): Promise<IBatch> => {
 export const updatesAllBatchInfo = async (
   id: number,
   product_name: string,
+  unit_buying_price: number,
+  unit_selling_price: number,
   expiration_date: Date
 ): Promise<IBatch> => {
   const queryText =
-    "UPDATE product_batches SET product_name=$1,expiration_date=$2 WHERE id=$3 RETURNING *";
+    "UPDATE product_batches SET product_name=$2, unit_buying_price=$3, unit_selling_price=$4, expiration_date=$5 WHERE id=$1 RETURNING *";
   const { rows } = await pool.query(queryText, [
     id,
     product_name,
+    unit_buying_price,
+    unit_selling_price,
     expiration_date,
   ]);
   return rows[0];
@@ -88,6 +102,26 @@ export const updatesBatchExpirationDate = async (
   const queryText =
     "UPDATE product_batches SET expiration_date=$1 WHERE id=$2 RETURNING *";
   const { rows } = await pool.query(queryText, [expiration_date, id]);
+  return rows[0];
+};
+
+export const updatesBatchUnitBuyingPrice = async (
+  id: number,
+  unit_buying_price: number
+): Promise<IBatch> => {
+  const queryText =
+    "UPDATE product_batches SET unit_buying_price=$1 WHERE id=$2 RETURNING *";
+  const { rows } = await pool.query(queryText, [unit_buying_price, id]);
+  return rows[0];
+};
+
+export const updatesBatchUnitSellingPrice = async (
+  id: number,
+  unit_selling_price: number
+): Promise<IBatch> => {
+  const queryText =
+    "UPDATE product_batches SET unit_selling_price=$1 WHERE id=$2 RETURNING *";
+  const { rows } = await pool.query(queryText, [unit_selling_price, id]);
   return rows[0];
 };
 
