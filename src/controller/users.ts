ebee62484fc190a758d94d191246bfbc8734e4db
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-05-26 22:57:54 -0300

    feat: Add functions to create admin, manager, stocker, and cashier users

--- src/controller/users.ts
+++ src/controller/users.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from "express";
-import { createsUser, authenticatesUser } from "../model/users";
+import {
+  createsUser,
+  authenticatesUser,
+  createsAdmin,
+  createsManager,
+  createsStocker,
+  createsCashier,
+} from "../model/users";
 
 export const createUser = async (req: Request, res: Response) => {
   try {
@@ -42,6 +49,62 @@ export const userLogin = async (req: Request, res: Response) => {
   } catch (error: any) {
     res.status(500).json({
       msg: "Error while authenticating user. Please, try again.",
+      error: error.message || "Unknown error",
+    });
+  }
+};
+
+export const createAdmin = async (req: Request, res: Response) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const newUser = await createsAdmin(name, email, password);
+    res.status(201).json({ msg: "Admin user created!", user: newUser });
+  } catch (error: any) {
+    res.status(500).json({
+      msg: "Error while creating user. Please, try again.",
+      error: error.message || "Unknown error",
+    });
+  }
+};
+
+export const createManager = async (req: Request, res: Response) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const newUser = await createsManager(name, email, password);
+    res.status(201).json({ msg: "Manager user created!", user: newUser });
+  } catch (error: any) {
+    res.status(500).json({
+      msg: "Error while creating user. Please, try again.",
+      error: error.message || "Unknown error",
+    });
+  }
+};
+
+export const createStocker = async (req: Request, res: Response) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const newUser = await createsStocker(name, email, password);
+    res.status(201).json({ msg: "Stocker user created!", user: newUser });
+  } catch (error: any) {
+    res.status(500).json({
+      msg: "Error while creating user. Please, try again.",
+      error: error.message || "Unknown error",
+    });
+  }
+};
+
+export const createCashier = async (req: Request, res: Response) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const newUser = await createsCashier(name, email, password);
+    res.status(201).json({ msg: "Cashier user created!", user: newUser });
+  } catch (error: any) {
+    res.status(500).json({
+      msg: "Error while creating user. Please, try again.",
       error: error.message || "Unknown error",
     });
   }
