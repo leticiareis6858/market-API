f6674fe40e1796d4e6169537290ba5ed4a989908
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-05-26 23:08:08 -0300

    feat: Update user registration routes and functions with authentication and role verification

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -7,15 +7,35 @@ import {
   createStocker,
   createCashier,
 } from "../controller/users";
+import { authMiddleware, verifyRoles } from "../middleware/authentication";
 
 const router = express.Router();
 
-router.post("/user/register", createUser);
+router.post("/user/register", authMiddleware, verifyRoles("admin"), createUser);
+
 router.post("/user/login", userLogin);
 
 router.post("/user/register/admin", createAdmin);
-router.post("/user/register/manager", createManager);
-router.post("/user/register/stocker", createStocker);
-router.post("/user/register/cashier", createCashier);
+
+router.post(
+  "/user/register/manager",
+  authMiddleware,
+  verifyRoles("admin"),
+  createManager
+);
+
+router.post(
+  "/user/register/stocker",
+  authMiddleware,
+  verifyRoles("admin", "manager"),
+  createStocker
+);
+
+router.post(
+  "/user/register/cashier",
+  authMiddleware,
+  verifyRoles("admin", "manager"),
+  createCashier
+);
 
 export { router as userRoutes };
