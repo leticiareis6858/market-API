b76dba22b4842ad928c7f33b737fc5a85ac9142f
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-05-26 23:00:21 -0300

    feat: Add routes for creating admin, manager, stocker, and cashier users

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,21 @@
 import express from "express";
-import { createUser, userLogin } from "../controller/users";
+import {
+  createUser,
+  userLogin,
+  createAdmin,
+  createManager,
+  createStocker,
+  createCashier,
+} from "../controller/users";
 
 const router = express.Router();
 
 router.post("/user/register", createUser);
 router.post("/user/login", userLogin);
+
+router.post("/user/register/admin", createAdmin);
+router.post("/user/register/manager", createManager);
+router.post("/user/register/stocker", createStocker);
+router.post("/user/register/cashier", createCashier);
 
 export { router as userRoutes };
